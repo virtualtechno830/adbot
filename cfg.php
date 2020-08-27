de8f3491674ee31f69fc481bda315600f01a6aee
virtualtechno830 <64238897+virtualtechno830@users.noreply.github.com>
2020-08-27 11:10:57 +0800

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,10 @@
+<?php
+
+/* Config adBOT.cc */
+// USER AGENT DI REKOMENDASIKAN DIGANTI DENGAN USER-AGENT MASING MASING !!!
+$useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36 Edg/84.0.522.50";
+$ssidzec = "";
+$ssidltc = "";
+$ssiddoge = "t1lcfo3v7uf442s96p1tpin4d7";
+$ssidbch = "";
+$valsurf = "SAsurf=SAsurfVal";
